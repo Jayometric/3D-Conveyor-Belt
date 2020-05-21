d620e979b15ffefaf116f14a606be996ac84e9fb
Jayometric <38576978+Jayometric@users.noreply.github.com>
2020-05-21 00:25:22 -0400

    Create ConveyorBelt.cs

---
+++ ConveyorBelt.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public List<GameObject> onBelt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i <= onBelt.Count -1; i++)
+        {
+            onBelt[i].GetComponent<Rigidbody>().velocity = speed * direction * Time.deltaTime;
+        }
+    }
+
+    // When something collides with the belt
+    private void OnCollisionEnter(Collision collision)
+    {
+        onBelt.Add(collision.gameObject);
+    }
+
+    // When something leaves the belt
+    private void OnCollisionExit(Collision collision)
+    {
+        onBelt.Remove(collision.gameObject);
+    }
+}
